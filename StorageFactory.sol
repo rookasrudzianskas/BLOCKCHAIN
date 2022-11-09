3ddbfad1b2f30b8535e6d977372659b5f379add1
Rokas <rookas.rudzenskas@gmail.com>
2022-11-09 12:03:55 +0200

    Rokas

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -13,12 +13,10 @@ contract StorageFactory {
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         // Address and ABI - application binary interface - how to interact with application
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        simpleStorage.store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
