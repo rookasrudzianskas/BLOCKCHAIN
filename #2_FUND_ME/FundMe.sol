8952748fbd7060ffd099cc66e6fe1ff29afe7038
Rokas <rookas.rudzenskas@gmail.com>
2022-11-09 15:17:16 +0200

    #2_FUND_ME: Optimization with immutable and constant
    - Optimization with immutable
    - Optimization with constant

--- #2_FUND_ME/FundMe.sol
+++ #2_FUND_ME/FundMe.sol
@@ -6,23 +6,28 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+    error NowOwner();
+
+// 940460
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
+    // optimization with constant
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // 1 * 10 ** 18
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
+    // optimization with immutable
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
         // want to be able to set minimum fund amount in USD
-        require(msg.value.getConversionRate() >= minimumUsd, "Did not send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Did not send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000
         // reverting, undos action before, and sends remaining gas back.
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
@@ -38,17 +43,6 @@ contract FundMe {
         funders = new address[](0);
         // actually withdraw the funds
 
-        // transfer 👇
-        // this refers to this contract if this errors, this will error, automatically reverts
-        // payable(msg.sender).transfer(address(this).balance);
-
-
-        // send
-        // with send, if it is more than 2300 in GAS. it will send a bool about success
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // if fails, we will revert
-        // require(sendSuccess, "Send Failed.");
-
         // call, if this returns data, it will be saved to dataRetured -- this is IMPORTANT AND BEST
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call Failed.");
@@ -56,8 +50,31 @@ contract FundMe {
 
     modifier onlyOwner {
         // with modfier first it does this, and then it does the function
-        require(msg.sender == owner, "Sender is not owner!");
+        if(msg.sender != i_owner) { revert NowOwner(); }
+        // require(msg.sender == i_owner, "Sender is not owner!");
         // Doing the rest of the code
         _;
     }
+
+    // what happens if someone sends this contract without calling fund function
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
 }
+
+
+
+// transfer 👇
+// this refers to this contract if this errors, this will error, automatically reverts
+// payable(msg.sender).transfer(address(this).balance);
+
+
+// send
+// with send, if it is more than 2300 in GAS. it will send a bool about success
+// bool sendSuccess = payable(msg.sender).send(address(this).balance);
+// if fails, we will revert
+//         require(sendSuccess, "Send Failed.");
