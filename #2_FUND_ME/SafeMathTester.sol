d96a37f2ec72386f31dcaa117df7207bac7073af
Rokas <rookas.rudzenskas@gmail.com>
2022-11-09 14:06:42 +0200

    Rokas

---
+++ #2_FUND_ME/SafeMathTester.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SafeMathTester {
+    uint8 public bigNumber = 255; // unchecked, if it is more, it is gonna start again
+
+    function add() public {
+    unchecked {bigNumber = bigNumber + 1;}
+    }
+
+}
