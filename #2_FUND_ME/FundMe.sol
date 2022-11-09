1f92bdc9f3fb0dc6d3549489955715c966610f28
Rokas <rookas.rudzenskas@gmail.com>
2022-11-09 14:40:45 +0200

    Rokas

---
+++ #2_FUND_ME/FundMe.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+// Get funds from user
+// Withdraw funds
+// Set minimum funding value in Usd
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18; // 1 * 10 ** 18
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // want to be able to set minimum fund amount in USD
+        require(msg.value.getConversionRate() >= minimumUsd, "Did not send enough!"); // 1e18 = 1 * 10 ** 18 == 1000000000000000
+        // reverting, undos action before, and sends remaining gas back.
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+        // brand new array, with zero objects in it
+        funders = new address[](0);
+        // actually withdraw the funds
+
+        // transfer 👇
+        // this refers to this contract if this errors, this will error, automatically reverts
+        // payable(msg.sender).transfer(address(this).balance);
+
+
+        // send
+        // with send, if it is more than 2300 in GAS. it will send a bool about success
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // if fails, we will revert
+        // require(sendSuccess, "Send Failed.");
+
+        // call, if this returns data, it will be saved to dataRetured -- this is IMPORTANT AND BEST
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed.");
+    }
+
+    modifier onlyOwner {
+        // with modfier first it does this, and then it does the function
+        require(msg.sender == owner, "Sender is not owner!");
+        // Doing the rest of the code
+        _;
+    }
+}
